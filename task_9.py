1a5c223fb403f04cded8ea02047dfc7c35fb2d53
toxidxd <toxid13@yandex.ru>
2022-10-10 03:30:03 +0300

    task_9 done

--- task_9.py
+++ task_9.py
@@ -1,8 +1,20 @@
 print('Задача 9. Выражение')
 
 
-#Дано число x.
-#Напишите программу для вычисления следующего выражения 
+# Дано число x.
+# Напишите программу для вычисления следующего выражения
 
 # ((x-1)(x-3)(x-7)…(x-63)/
-# ((x-2)(x-4)(x-8)…(x-64)) 
+# ((x-2)(x-4)(x-8)…(x-64))
+
+x = int(input("x = "))
+
+numerator = 1
+denominator = 1
+
+for num in range(1, 7):
+    numerator *= (x - (2 ** num - 1))
+    denominator *= (x - 2 ** num)
+
+res = numerator / denominator
+print(res)
